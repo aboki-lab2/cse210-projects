2807b8fd352d60adf9d37d9e8a3e1e1febd20c86
Paul Arungwa <parungwa@byui.edu>
2023-10-27 23:09:36 +0100

    edit3

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("Welcome Apostle Paul!");
+        Console.WriteLine("This is the right Job for you!");
+        Console.Write("What is your name, please!");
+        string name = Console.ReadLine();
+        Console.WriteLine($"Your name is {name}");
     }
 }
