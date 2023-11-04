1afa9993fb47d0322756dc5883f29c4349a54079
Paul Arungwa <parungwa@byui.edu>
2023-11-04 19:36:33 +0100

    edit

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -3,11 +3,13 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Welcome Apostle Paul!");
-        Console.WriteLine("This is the right Job for you!");
-        Console.Write("What is your name, please!");
-        string name = Console.ReadLine();
-        Console.WriteLine($"Your name is {name}");
+{
+        Console.Write("What is your first name? ");
+        string first = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string last = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {last}, {first} {last}.");
     }
 }
